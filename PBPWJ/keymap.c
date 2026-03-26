f45989e2707363f99f790b6e261b99a164e99744
keita <keitam506@gmail.com>
2026-03-26 20:48:14 +0900

    keys override

--- PBPWJ/keymap.c
+++ PBPWJ/keymap.c
@@ -489,9 +489,8 @@ uint16_t layer_state_set_user(uint16_t state) {
 
 // Custom QMK here
 const key_override_t *key_overrides[] = {
-	&ko_make_with_layers(MOD_MASK_SHIFT, JP_SCLN, JP_COLN, 6),
-    &ko_make_with_layers(MOD_MASK_SHIFT, JP_QUOT, JP_DQT, 6),
-    &ko_make_with_layers(MOD_MASK_SHIFT, JP_COMM, JP_LT, 6),
-    &ko_make_with_layers(MOD_MASK_SHIFT, JP_DOT, JP_GT, 6),
-    &ko_make_with_layers(MOD_MASK_SHIFT, JP_SLSH, JP_QUES, 6)
+    // ; -> :
+	&ko_make_with_layers(MOD_MASK_SHIFT, JP_SCLN, JP_COLN, 5),
+    // ' -> "
+    &ko_make_with_layers(MOD_MASK_SHIFT, JP_QUOT, JP_DQUO, 5)
 };
